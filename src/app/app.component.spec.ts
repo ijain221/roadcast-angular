b0690eb4d1305c775bb3f5d8b0c820edead472a4
ijain221 <ijain221@gmail.com>
2021-03-11 22:50:46 +0530

    initail

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,7 +19,6 @@ describe('AppComponent', () => {
   it(`should have as title 'roadcast-angular'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('roadcast-angular');
   });
 
   it('should render title', () => {
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {DateService} from './date.service'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'roadcast-angular';
+
+export class AppComponent implements OnInit {
+  date: any
+  constructor(public DateService: DateService){}
+  
+
+  ngOnInit() {
+    this.getDate()
+  }
+
+  getDate(){
+  this.date = this.DateService.getDate()
+   console.log(this.date)
+  }
+  
 }
+
+
